7ed2dcb587041b447ae3792c6d64401790475ad8
SJP\sjp <sungjun429@hanmail.net>
2017-07-18 14:05:18 +0900

    5test

--- src/coin/controller/CoinController.java
+++ src/coin/controller/CoinController.java
@@ -12,11 +12,8 @@ import coin.model.dto.BtcDTO;
 
 public class CoinController extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		process(request, response);
-	}
-	
-	protected void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("euc-kr");
+		System.out.println("----------  -------");
+		request.setCharacterEncoding("euc-kr");   
 		String command = request.getParameter("command");
 		try{
 			if(command.equals("btcAll")){					// 모든 비트코인 정보 검색
@@ -24,7 +21,7 @@ public class CoinController extends HttpServlet {
 			}else if(command.equals("btc")){			// 날짜별 비트코인 검색
 				btc(request, response);
 			}else if(command.equals("btcInsert")){	// 비트코인 추가 등록
-				btcInsert(request, response);
+				btcInsert(request, response);   
 			}
 		}catch(Exception s){
 			request.setAttribute("errorMsg", s.getMessage());
@@ -57,25 +54,30 @@ public class CoinController extends HttpServlet {
 		request.getRequestDispatcher(url).forward(request, response);
 	}
 	
-
 	// 비트코인 데이터 삽입
 	protected void btcInsert(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String url = "showError.jsp";
+		String d = request.getParameter("data");
+		String[] data = d.split(",");
+		String date = null;
+		int price, volume, eid;
 		
-		String date = request.getParameter("date");
-		int lastPrice = request.getParameter("lastPrice");
-		int volume = request.getParameter("volume");
-		int eid = request.getParameter("eid");
-		
-		BtcDTO btc = new BtcDTO(date, lastPrice, volume, eid);
 		try{
-			boolean result = CoinService.addBtc(btc);
-			if(result){
-				request.setAttribute("btc", btc);
-				request.setAttribute("successMsg", "삽입 완료");
-				url = "btcDetail.jsp";
-			}else{
-				request.setAttribute("errorMsg", "다시 시도하세요");
+			for (int i = 0; i < data.length; i++) {
+				date = data[i].split("/")[0];
+				price = Integer.parseInt(data[i].split("/")[1]);
+				volume = Integer.parseInt(data[i].split("/")[2]);
+				eid = Integer.parseInt(data[i].split("/")[3]);
+				BtcDTO btc = new BtcDTO(date, price, volume, eid);
+				boolean result = CoinService.addBtc(btc);
+				
+				if(result){
+					request.setAttribute("btc", btc);
+					request.setAttribute("successMsg", "삽입 완료");
+					//url = "btcDetail.jsp";
+				}else{
+					request.setAttribute("errorMsg", "다시 시도하세요");
+				}
 			}
 		}catch(Exception s){
 			request.setAttribute("errorMsg", s.getMessage());
